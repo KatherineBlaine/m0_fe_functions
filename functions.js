e8b5b7cc49413a6f0d1fe2cbc25c4ba2e232c6ba
Katherine Blaine <blaine.katherine12@gmail.com>
2022-10-22 11:58:17 -0600

    Update functions assignment response

--- functions.js
+++ functions.js
@@ -1,38 +1,38 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
-// function greetUser() {
-//     console.log("Hello, User. Its great to see you!")
-// }
+function greetUser() {
+    console.log("Hello, User. Its great to see you!")
+}
 
-// for (var i = 0; i < 3; i++) {
-// greetUser();
-// }
+for (var i = 0; i < 3; i++) {
+greetUser();
+}
 
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
-// function greetUser(name) {
-//     console.log(`Hello, ${name}. Its great to see you!`)
-// }
+function greetUser(name) {
+    console.log(`Hello, ${name}. Its great to see you!`)
+}
 
-// greetUser("Katherine")
+greetUser("Katherine")
 
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will 
 // represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name 
 // of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
-// function evaluateSalaryRange (_company, num1, num2) {
-//     var num = num2 - num1;
-//     return num;
-// }
+function evaluateSalaryRange (_company, num1, num2) {
+    var num = num2 - num1;
+    return num;
+}
 
-// var name = "Turing"
-// var highSalary = 10000000
-// var lowSalary = 100000
-// var salaryRange = evaluateSalaryRange(name, lowSalary, highSalary)
+var name = "Turing"
+var highSalary = 10000000
+var lowSalary = 100000
+var salaryRange = evaluateSalaryRange(name, lowSalary, highSalary)
 
-// console.log(`The company ${name} has a pay range of ${salaryRange}`)
+console.log(`The company ${name} has a pay range of ${salaryRange}`)
 
 // 4: Write a function that satifies the following interaction pattern:
 
@@ -47,7 +47,6 @@
 
 // checkStock(1, "Salsa");
 // => "Salsa - running LOW"
-
 
 
 function checkStock(num, _product){
